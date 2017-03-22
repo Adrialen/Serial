b5cd9d068b41b8ad22935079b34d16595cac5f30
Adrià Len <senseilang87@gmail.com>
2017-03-22 12:18:42 +0100

    Serial 5.1-5.3

---
+++ Serial/Serial_5.2/Serial_5.2.ino
@@ -0,0 +1,55 @@
+/************************************************************************************************************************
+**                                                                                                                     ** 
+**                                                      Serial 5.2                                                     ** 
+**                                                                                                                     **
+**                                                                                                                     **
+**                                                                                                                     **                                                                                                        
+************************************************************************************************************************/
+
+/*********** Includes  ***********************************************************/
+
+
+
+//***********  Variables  *********************************************************
+float numParell, numSenar;        
+float resParell, resSenar;
+
+//***********  Setup  *************************************************************
+void setup() {
+ Serial.begin(9600); // initialize serial
+ Serial.print("Entrar un numero, ");
+}
+
+
+//***********  Loop  **************************************************************
+void loop() {
+ while (Serial.available() > 0) { 
+
+    numParell = Serial.parseInt();
+    Serial.print("el ");
+    Serial.print(numParell);
+    Serial.print(" es parell."); 
+
+    numSenar = Serial.parseInt();
+    Serial.print("el ");
+    Serial.print(numSenar);
+    Serial.println(" es senar.");
+    
+    if (Serial.read() == '\n') {                    
+      
+    numParell / 2;
+    numSenar / 3;                    
+
+    Serial.print("el ");
+    Serial.print(numParell);
+    Serial.print(" es parell."); 
+
+    Serial.print("el ");
+    Serial.print(numSenar);
+    Serial.println(" es senar.");
+    
+    Serial.print("Entrar un nou mumero, ");
+    }
+  } 
+}
+
---
+++ Serial/serial_5.3/serial_5.3.ino
@@ -0,0 +1,59 @@
+/************************************************************************************************************************
+**                                                                                                                     ** 
+**                                                      Serial 5.3                                                     ** 
+**                                                                                                                     **
+**                                                                                                                     **
+**                                                                                                                     **                                                                                                        
+************************************************************************************************************************/
+
+//********** INCLUDE **************************************************************
+
+//********** VARIABLES ************************************************************
+int n1;           
+
+//********** SETUP ****************************************************************
+void setup() 
+{                         // S'executa un sol cop
+  Serial.begin(9600);     // Configura una biblioteca a 9600 bpm
+  Serial.println ("Entra un numero");   // Escriu  (Entra un numero)
+  
+}
+//********** LOOP *****************************************************************
+ void loop ()
+ {
+   while (Serial.available() > 0) {      // Llegeix els números del Serial
+    n1 = Serial.parseFloat(); 
+    Serial.print ("El numero ");         // Escriu a una nova línia (El numero )
+    Serial.print (n1);                   // Escriu el valor de "n1"
+    if (n1 / 10 == 0)            // Si divideixes entre 10 n1 i dona 0 ...
+      {
+      Serial.println (" te 1 xifra");    // Escriu ( te 1 xifra)
+      }     
+    else if (n1 / 100 == 0)      // Si fas n1 entre 100 i dona 0 ...
+      {   
+      Serial.println (" te 2 xifres");   // Escriu ( te 2 xifres)
+      }
+    else if (n1 / 1000 == 0)     // Si fas n1 entre 1000 i dona 0 ...
+      {
+      Serial.println (" te 3 xifres");   // Escriu ( te 3 xifres)
+      }
+    else if (n1 / 10000 == 0)    // Si fas n1 entre 10000 i dona 0 ...
+      {
+      Serial.println (" te 4 xifres");   // Escriu ( te 4 xifres)
+      }
+    else if (n1 / 100000 == 0)   // Si fas n1 100000 i dona 0 ...
+      {
+      Serial.println (" te 5 xifres");   // Escriu ( te 5 xifres)
+      }
+    else if (n1 / 1000000 == 0)  // Si fas n1 entre 1000000 i dona 0 ...
+      {
+      Serial.println (" te 6 xifres");   // Escriu ( te 6 xifres)
+      }
+      
+    if (Serial.read() == '\n')
+    Serial.println (" ");                       // Escriu ( )
+    Serial.println ("Entra un altre numero");   
+                          // Escriu a la següent linia (Entra un altre numero)
+  }
+ }
+
